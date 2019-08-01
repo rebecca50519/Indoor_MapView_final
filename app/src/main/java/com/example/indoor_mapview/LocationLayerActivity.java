ea82f9bb7cca67ce5a1a92b1f6ef4bbdc6bf7057
rebecca50519 <rebecca50519@gmail.com>
2019-08-01 14:32:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/indoor_mapview/LocationLayerActivity.java
@@ -0,0 +1,112 @@
+package com.example.indoor_mapview;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PointF;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.onlylemi.mapview.library.MapView;
+import com.onlylemi.mapview.library.MapViewListener;
+import com.onlylemi.mapview.library.layer.LocationLayer;
+
+import java.io.IOException;
+
+public class LocationLayerActivity extends AppCompatActivity implements SensorEventListener {
+
+    private MapView mapView;
+
+    private LocationLayer locationLayer;
+
+    private boolean openSensor = false;
+
+    private SensorManager sensorManager;
+    private Sensor mAccelerometer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_layer_test);
+
+        mapView = (MapView) findViewById(R.id.mapview);
+
+        Bitmap bitmap = null;
+        try {
+            bitmap = BitmapFactory.decodeStream(getAssets().open("pic.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mapView.loadMap(bitmap);
+        mapView.setMapViewListener(new MapViewListener() {
+            @Override
+            public void onMapLoadSuccess() {
+                locationLayer = new LocationLayer(mapView, new PointF(400, 150));
+                locationLayer.setOpenCompass(true);
+                //locationLayer.setCompassIndicatorCircleRotateDegree(60);
+                //locationLayer.setCompassIndicatorArrowRotateDegree(-30);
+                Log.d("msg","start");
+                mapView.addLayer(locationLayer);
+
+                mapView.refresh();
+            }
+
+            @Override
+            public void onMapLoadFail() {
+
+            }
+
+        });
+
+        Log.d("msg","oncreate");
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor
+                (Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("msg","onResume");
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor
+                (Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_NORMAL);
+        openSensor = !openSensor;
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Log.d("msg","onsensor");
+        if (mapView.isMapLoadFinish() && openSensor) {
+            float mapDegree = 0; // the rotate between reality map to northern
+            float degree = 0;
+            /*if (event.sensor.getType() == Sensor.TYPE_ORIENTATION) {
+
+
+            }*/degree = event.values[0];
+            Log.d("msg","degree="+degree);
+            locationLayer.setCompassIndicatorCircleRotateDegree(-degree);
+            locationLayer.setCompassIndicatorArrowRotateDegree(mapDegree + mapView
+                    .getCurrentRotateDegrees() + degree);
+            mapView.refresh();
+            Log.d("msg","degree="+degree);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        sensorManager.unregisterListener(this);
+    }
+
+}
---
+++ app/src/main/java/com/example/indoor_mapview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.indoor_mapview;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent=new Intent();
+        intent.setClass(this, Main2Activity.class);
+        startActivity(intent);
+    }
+}
